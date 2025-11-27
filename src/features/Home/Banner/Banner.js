c0e006d0bc49afec72c933e321946e7a62f03477
CabralArthur <arthurcabral.dev@gmail.com>
2025-11-27 11:06:38 -0300

    feat: update page title and improve banner image attributes; modify text in Home component

--- src/features/Home/Banner/Banner.js
+++ src/features/Home/Banner/Banner.js
@@ -12,7 +12,7 @@ const Banner = () => {
 		<Grid grow gutter="sm">
             <Grid.Col lg="12">
                 <Paper className={classes.bannerContainer} radius="md" bg="red.9">
-                    <Image className={classes.bannerImage} fit="contain" src={bannerHome} alt="Banner" />
+                    <Image className={classes.bannerImage} fit="contain" radius="md" src={bannerHome} alt="Banner" />
                 </Paper>
             </Grid.Col>
 		</Grid>
